982febf49106361fc5522b9276d9db0f31c83a0f
dhanya-mary <Dhanya.Mary.Biju@gds.ey.com>
2025-11-26 12:51:02 +0530

    routing, login screen and admin layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Admin } from './admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Login },
+  { path: 'admin', component: Admin },
+  { path: '**', redirectTo: '' },
+];
---
+++ frontend/src/app/common/dialog/dialog.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-dialog',
+  standalone: true,
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './dialog.html',
+  styleUrls: ['./dialog.scss'],
+})
+export class Dialog {
+  constructor(public dialogRef: MatDialogRef<Dialog>, @Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  onCancel() {
+    if (this.data.resetForm) {
+      this.data.resetForm();
+    }
+    this.dialogRef.close(false);
+  }
+}
