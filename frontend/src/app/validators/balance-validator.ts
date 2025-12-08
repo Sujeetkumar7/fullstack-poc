e8d0a0700eb78252d4b1454c00fade1b4fc050eb
jyothi1439 <karukujyothi@gmail.com>
2025-12-08 12:05:32 +0530

    stocks pop up changes

--- frontend/src/app/validators/balance-validator.ts
+++ frontend/src/app/validators/balance-validator.ts
@@ -9,3 +9,15 @@ export function balanceValidator(balance: number) {
     return null;
   };
 }
+
+export function balanceValidationForStocks(balance: number, pricePerUnit: number) {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const quantity = control.get('quantity')?.value;
+    const amount = quantity * pricePerUnit;
+
+    if (quantity && amount > balance) {
+      return { insufficientBalance: true };
+    }
+    return null;
+  };
+}
